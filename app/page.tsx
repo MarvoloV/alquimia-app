b94239fe00547be00a3eea2d03c695c527d32199
Jorge Luis Adco Ccahuay <marvolov@MacBook-Pro-de-Jorge.local>
2025-06-22 18:30:05 -0500

    hotfix: resolve redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("http://localhost:3000/auth/login");
+  redirect("/auth/login");
 
   return <main className="min-h-screen flex flex-col items-center "></main>;
 }
